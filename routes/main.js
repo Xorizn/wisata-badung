b790c64a5a969ee537e5d34163b6dd8d15f69603
Bala Krishna <86514883+Xorizn@users.noreply.github.com>
2024-05-31 20:51:07 +0800

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ __path = process.cwd();
 
 var express = require("express");
 var router = express.Router();
-var { data_galery } = require("../config");
+var { data_galery, penginapan, restoran } = require("../config");
 var { data, datas } = require("../data");
 
 function createSlug(title) {
@@ -19,7 +19,17 @@ router.get("/", (req, res) => {
   const rating_tinggi = topRatedWisatas.slice(0, 3);
   const data_home = datas.slice(0, 10);
 
-  res.render("index", { nav: "/", rating_tinggi, data_home });
+  const bar = restoran.slice(0, 3);
+  const ber = penginapan.slice(0, 3);
+
+  res.render("index", {
+    nav: "/",
+    rating_tinggi,
+    data_home,
+    data,
+    restoran: bar,
+    penginapan: ber,
+  });
 });
 router.get("/wisata-detail", (req, res) => {
   res.render("wisata-detail", { nav: "/" });
@@ -31,41 +41,158 @@ router.get("/about", (req, res) => {
   res.render("about", { nav: "about" });
 });
 
+// router.get("/pariwisata", (req, res) => {
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = 10;
+//   const startIndex = (page - 1) * limit;
+//   const endIndex = page * limit;
+
+//   const results = {};
+//   if (endIndex < data.length) {
+//     results.next = {
+//       page: page + 1,
+//       limit: limit,
+//     };
+//   }
+//   if (startIndex > 0) {
+//     results.previous = {
+//       page: page - 1,
+//       limit: limit,
+//     };
+//   }
+//   results.results = data.slice(startIndex, endIndex);
+//   res.render("wisatas", {
+//     wisatas: results.results,
+//     next: results.next,
+//     previous: results.previous,
+//     nav: "pariwisata",
+//   });
+// });
+
 router.get("/pariwisata", (req, res) => {
-  const page = parseInt(req.query.page) || 1;
+  const { search, page = 1 } = req.query;
+  const limit = 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  
+  if (search) {
+    // Filter data berdasarkan query pencarian
+    const filteredData = data.filter(
+      (item) =>
+        item.title.toLowerCase().includes(search.toLowerCase()) ||
+        item.desc.toLowerCase().includes(search.toLowerCase())
+    );
+
+    res.render("data_search", {
+      wisatas: filteredData,
+      nav: "pariwisata",
+    });
+  } else {
+    const results = {};
+    if (endIndex < data.length) {
+      results.next = {
+        page: parseInt(page) + 1,
+        limit: limit,
+      };
+    }
+    if (startIndex > 0) {
+      results.previous = {
+        page: parseInt(page) - 1,
+        limit: limit,
+      };
+    }
+    results.results = data.slice(startIndex, endIndex);
+    res.render("wisatas", {
+      wisatas: results.results,
+      next: results.next,
+      previous: results.previous,
+      nav: "pariwisata",
+    });
+  }
+});
+
+router.get("/penginapan", (req, res) => {
+  const { search, page = 1 } = req.query;
   const limit = 10;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
 
   const results = {};
-  if (endIndex < data.length) {
+  if (endIndex < penginapan.length) {
     results.next = {
-      page: page + 1,
+      page: parseInt(page) + 1,
       limit: limit,
     };
   }
   if (startIndex > 0) {
     results.previous = {
-      page: page - 1,
+      page: parseInt(page) - 1,
       limit: limit,
     };
   }
-  results.results = data.slice(startIndex, endIndex);
-  res.render("wisatas", {
+  results.results = penginapan.slice(startIndex, endIndex);
+  res.render("hotel", {
     wisatas: results.results,
     next: results.next,
     previous: results.previous,
-    nav: "pariwisata",
+    nav: "penginapan",
+  });
+});
+
+router.get("/restoran", (req, res) => {
+  const { search, page = 1 } = req.query;
+  const limit = 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const results = {};
+  if (endIndex < restoran.length) {
+    results.next = {
+      page: parseInt(page) + 1,
+      limit: limit,
+    };
+  }
+  if (startIndex > 0) {
+    results.previous = {
+      page: parseInt(page) - 1,
+      limit: limit,
+    };
+  }
+  results.results = restoran.slice(startIndex, endIndex);
+  res.render("restoran", {
+    wisatas: results.results,
+    next: results.next,
+    previous: results.previous,
+    nav: "restoran",
   });
 });
 
 router.get("/pariwisata/:slug", (req, res) => {
   const wisataSlug = req.params.slug;
   const wisata = data.find((w) => createSlug(w.title) === wisataSlug);
+
   if (wisata) {
     res.render("wisata-detail", { wisata, nav: "pariwisata" });
   } else {
     res.status(404).render("404", { nav: "pariwisata" });
+  }
+});
+router.get("/penginapan/:slug", (req, res) => {
+  const wisataSlug = req.params.slug;
+  const wisata = penginapan.find((w) => createSlug(w.nama) === wisataSlug);
+  if (wisata) {
+    res.render("hotel-detail", { wisata, nav: "penginapan" });
+  } else {
+    res.status(404).render("404", { nav: "penginapan" });
+  }
+});
+router.get("/restoran/:slug", (req, res) => {
+  const wisataSlug = req.params.slug;
+  const wisata = restoran.find((w) => createSlug(w.nama) === wisataSlug);
+  if (wisata) {
+    res.render("restoran-detail", { wisata, nav: "restoran" });
+  } else {
+    res.status(404).render("404", { nav: "restoran" });
   }
 });
 
